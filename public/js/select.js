bb225789e744d7001fcae9a2f0b8bec48fc79aa8
Christophe-THEVENET <christophethevenet2.0@gmail.com>
2025-06-24 06:35:42 +0200

    feat: update multiple select from jquery to choice.js

--- public/js/select.js
+++ public/js/select.js
@@ -1,3 +1,10 @@
-$(document).ready(function() {
-    $('select[multiple]').select2();
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.js-choice').forEach(function (element) {
+        new Choices(element, {
+            removeItemButton: true, // Affiche la croix pour retirer un élément sélectionné
+            searchEnabled: true, // Active la recherche
+            placeholder: true,
+            placeholderValue: element.getAttribute('placeholder') || ''
+        });
+    });
 });
